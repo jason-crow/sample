5edf5853998c73b85a8735f7228bf4fa97a026fd
Jason Crow <Jason.Crow@bentley.com>
2017-02-16 09:19:36 -0600

    random quotes

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,10 @@
-(function(){
-	console.log('hi');
-})();
+$(document).ready(function(){
+	nextQuote();	
+});
+
+function nextQuote() {
+	$.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?", function(json) {
+	  $("h1").text(json.quoteText);
+	  $("p").text(json.quoteAuthor);
+	});
+}
